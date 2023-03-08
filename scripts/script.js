fbf502d5ceae542fa4bae43c42c320824b09ef81
felipedorea <122095865+felipedorea@users.noreply.github.com>
2023-03-08 07:14:52 -0300

    .

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+      items: 1,
+      autoplay: true,
+      autoplayTimeout: 6000,
+      autoplaySpeed: 1500,
+      loop: true,
+      dots: false,
+    });
+  });
